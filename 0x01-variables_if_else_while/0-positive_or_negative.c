8ba7cd4889fb29c3f407f664a7c93e162a7bcb67
mlinami <imanim791@gmail.com>
2022-07-04 12:51:57 -0700

    Fix 6

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,18 +2,18 @@
 #include <stdio.h>
 #include <time.h>
 
-/* main --> indicates that the main() function will not return any value, but the int main() indicates that the main() can return integer type data.*/
+/* main --> indicates that the main() function will not return any value*/
 int main(void)
 {
 int n;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-if (n > 0 )
+if (n > 0)
 	printf("%d is positive\n", n );
 if (n == 0)
   	printf( "%d is zero\n", n );
-if (n < 0 )
-	 printf( "%d is negative\n ", n );
+if (n < 0)
+	 printf( "%d is negative\n", n );
 return (0);
 }
