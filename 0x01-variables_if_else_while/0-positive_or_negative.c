c1f343033ff278ee68fc40c5654c53a0cc158dba
mlinami <imanim791@gmail.com>
2022-07-04 10:48:32 -0700

    C variables 1

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+/* more headers goes there */
+
+
+
+/* betty style doc for function main goes there */
+
+int main(void)
+
+{
+
+		int n;
+		if (n>0)
+		{
+			printf("is positive/n");
+	        }
+
+		if (n == 0)
+                {
+			printf( " is zero/n ");
+		}
+
+		if (n<0)
+		{
+		   printf( " is negative/n ");
+		}
+}
