9455e476580d4f5e98471bb978c6c5f13016e025
mlinami <imanim791@gmail.com>
2022-07-04 11:48:38 -0700

    Rectifying errors

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,7 +7,6 @@
 
 
 /* betty style doc for function main goes there */
-
 int main(void)
     
 {
@@ -15,24 +14,20 @@ int main(void)
 
 
 		srand(time(0));
-
-			n = rand() - RAND_MAX / 2;
+		n = rand() - RAND_MAX / 2;
 
 		
-		if (n>0)
+		if (n > 0 )
 		{
-			printf("is positive/n");
+			printf("is positive");
 	        }
-
 		if (n == 0)
                 {
-			printf( " is zero/n ");
+			printf( " is zero");
 		}
-
-		if (n<0)
+		if (n < 0 )
 		{
-		   printf( " is negative/n ");
+		   printf( " is negative ");
 		}
-		
 		return (0);
 }
