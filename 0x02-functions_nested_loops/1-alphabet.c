86dc185c84ad221cff636b3d0cf6d15c22c3d69e
mlinami <imanim791@gmail.com>
2022-07-06 12:49:21 -0700

    Print lowercase alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * 
+ *print_alphabet - prints lowercase alphabet
+ *
+ */
+void print_alphabet(void);
+{ 
+	int i;
+	for(i = "a"; i <= "z"; i++)
+	{
+		_putchar(j);
+	}
+	_putchar("\n");
+}
