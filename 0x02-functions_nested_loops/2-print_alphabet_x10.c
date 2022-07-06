6fdef0f7af99a8f7879c87b3db7d09d173f9d7b2
mlinami <imanim791@gmail.com>
2022-07-06 15:25:52 -0700

    HOT FIX

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,10 +9,10 @@ void print_alphabet_x10(void)
 	char i;
 	char l;
 
-	for (i = 0; l <= 10; l++)
-	{
+	for (l = 0; l <= 10; l++)
+{
 		for (i = 'a'; i <= 'z'; i++)
 		_putchar(i);
 		_putchar('\n');
-	}		
+}		
 }
