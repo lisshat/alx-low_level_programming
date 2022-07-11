203dc51a592dbc6bab45c45b5bcc1f03ebcddb0f
mlinami <imanim791@gmail.com>
2022-07-11 10:37:33 -0700

    Pointer swap values

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *swap_int - swaps values of given two integers
+ *@a:parameter 1
+ *@b:parameter 2
+ */
+void swap_int(int *a, int *b);
+{
+	int a;
+	int b;
+	int m;
+	int n;
+
+	a = m;
+	b = n;
+	*a = b
+	*b = a
+}
