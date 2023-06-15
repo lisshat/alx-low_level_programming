04f315a54b9c959fb5ceee254b12b452814738da
lisshat <mucheraimani@gmail.com>
2023-06-15 05:05:08 -0700

    main

--- 4-puts.c
+++ 4-puts.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
-
 /**
- * main - prints "Programming is like building a multilingual
- * puzzle,followed by a new line
+ * main - A program that prints line with puts function
+ * 
  * Return - Always 0.
  */
 
