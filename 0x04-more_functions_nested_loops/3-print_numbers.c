12bf13d1d774339b7acd98423d211550e5dab09a
lisshat <mucheraimani@gmail.com>
2023-06-24 09:58:11 -0700

    fix

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,6 +2,7 @@
 
 /**
  * print_numbers - Prints the numbers from 0 to 9, followed by a new line.
+ * retutn void 
  */
 void print_numbers(void)
 {
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,1 +1,3 @@
 1-isdigit.c
+2-mul.c
+3-printnumbers.c
